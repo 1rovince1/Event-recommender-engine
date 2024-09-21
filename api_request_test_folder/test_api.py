acd0d7cf53586937c48a56776f4936c1808111de
Rovince Gangwar <rovince.gangwar@unthinkable.co>
2024-09-22 00:23:42 +0530

    final setup and first commtit

---
+++ api_request_test_folder/test_api.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from typing import Dict, Any
+import json
+
+app = FastAPI()
+
+@app.get('/event_data', response_model = Dict[str, Any])
+async def get_event_data():
+    with open('data/all_events.json', 'r') as file:
+        json_obj_1 = json.load(file)
+
+    return json_obj_1
+
+@app.get('/user_data', response_model = Dict[str, Any])
+async def get_user_data():
+    with open('data/new_event_user_data.json', 'r') as file:
+        json_obj_2 = json.load(file)
+
+    return JSONResponse(content=json_obj_2)
